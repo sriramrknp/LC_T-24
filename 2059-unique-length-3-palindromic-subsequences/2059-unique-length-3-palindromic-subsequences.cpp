0702c70eb7669a9bdb10ca4bb43899b4dea7899a
Sriram Reddy <77890339+sriramrknp@users.noreply.github.com>
2025-01-09 15:24:02 +0530

    Time: 171 ms (40.94%), Space: 36.4 MB (8.7%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
@@ -1,31 +1,29 @@
 class Solution {
-public:
+    int cal(string s, int i, int j, int ans) {
+        unordered_map<char,int> temp;
+
+        for(int t=i; t <= j; t++) {
+            temp[s[t]]++;
+        }
+
+        ans += temp.size();
+        return ans;
+    }
+public: 
     int countPalindromicSubsequence(string s) {
-        unordered_map<char,int> charBegin, charEnd, temp;
+        unordered_map<char,int> charBegin;
         int ans = 0, n = s.size();
 
         for(int i=0;i<n;i++) {
-            if(charBegin.find(s[i]) == charBegin.end()) 
+            if(charBegin.find(s[i]) == charBegin.end())
                 charBegin[s[i]] = i;
         }
-        for(int i=n-1; i>=0; i--) {
-            if(charEnd.find(s[i]) == charEnd.end() and charBegin[s[i]] != i) 
-                charEnd[s[i]] = i;
-        }
-        // for(auto [c,i]:charBegin) cout<<c<<" "<<i<<endl;
-        // cout<<endl;
-        // for(auto [c,i]:charEnd) cout<<c<<" "<<i<<endl;
 
-        for(auto [c,i]:charBegin) {
-            int j = i;
-            if(charEnd[c] > i) {
-                j = charEnd[c];
-                
-                for(int t=i+1; t < j; t++) {
-                    temp[s[t]]++;
-                }
-                ans += temp.size();
-                temp.clear();
+        for(int i=n-1; i>=0; i--) {
+            if(charBegin.find(s[i]) != charBegin.end() and charBegin[s[i]] != -1 and charBegin[s[i]] != i) 
+            {
+                ans += cal(s, charBegin[s[i]]+1, i-1, 0);
+                charBegin[s[i]] = -1;
             }
         }
         
