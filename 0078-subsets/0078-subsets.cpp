2db840ee7eb4ee71471959baa13e72e5bf7ce5e1
Sriram Reddy <77890339+sriramrknp@users.noreply.github.com>
2025-02-04 16:59:47 +0530

    Time: 0 ms (100%), Space: 10 MB (46.95%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -2,23 +2,27 @@ typedef vector<int> vi;
 typedef vector<vector<int>> vvi;
 #define PB push_back
 #define POPB pop_back
+#define sze nums.size()
 
 class Solution {
-    vi setProcess;
-    void takeOrNotTake(vi nums, int i, vvi& ans, int n) {
-        if(i >= n) {
-            ans.PB(setProcess);
-            return;
-        }
-        takeOrNotTake(nums, i+1, ans, n);
-        setProcess.PB(nums[i]);
-        takeOrNotTake(nums, i+1, ans, n);
-        setProcess.POPB();
-    }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vvi ans;
-        takeOrNotTake(nums, 0, ans, nums.size());
+        int n = sze;
+        // Another way to generate subsets is based on the 
+        // bit representation of integers.
+        // Each subset of a set of n elements can be 
+        // represented as a sequence of n bits,
+        // which corresponds to an integer between 0...2n − 1. 
+        // The ones in the bit sequence
+        // indicate which elements are included in the subset.
+        for (int b = 0; b < (1<<n); b++) {
+            vector<int> subset;
+            for (int i = 0; i < n; i++) {
+                if (b&(1<<i)) subset.PB(nums[i]);
+            }
+            ans.PB(subset);
+        }
         return ans;
     }
 };
