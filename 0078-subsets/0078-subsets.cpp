6eb9e062d6e53c340b47efd91b56116436758428
Sriram Reddy <77890339+sriramrknp@users.noreply.github.com>
2025-06-11 15:16:44 +0530

    Time: 0 ms (100%), Space: 9.8 MB (80.97%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,28 +1,17 @@
-typedef vector<int> vi;
-typedef vector<vector<int>> vvi;
-#define PB push_back
-#define POPB pop_back
-#define sze nums.size()
-
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vvi ans;
-        int n = sze;
-        // Another way to generate subsets is based on the 
-        // bit representation of integers.
-        // Each subset of a set of n elements can be 
-        // represented as a sequence of n bits,
-        // which corresponds to an integer between 0...2n − 1. 
-        // The ones in the bit sequence
-        // indicate which elements are included in the subset.
-        for (int b = 0; b < (1<<n); b++) {
-            vector<int> subset;
-            for (int i = 0; i < n; i++) {
-                if (b&(1<<i)) subset.PB(nums[i]);
+        int n = nums.size();
+        vector<vector<int>>res;
+        for(int i=0;i<(1<<n);i++){
+            vector<int>row;
+            for(int j=0;j<n;j++){
+                if(i&(1<<j)){
+                    row.push_back(nums[j]);
+                }
             }
-            ans.PB(subset);
-        }
-        return ans;
+            res.push_back(row);
+        }   
+        return res;
     }
 };
